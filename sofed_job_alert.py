ab1dcc43a15d6662572f5dc52767fa13066210c5
Anupam Pal <infoanupampal@gmail.com>
2026-03-12 02:01:52 +0530

    Refactor Python code due to GitHub action trigger
    Removing
    while True:
        time.sleep(3600) # 1 hour

--- sofed_job_alert.py
+++ sofed_job_alert.py
@@ -1,55 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
-import time
+import os
 
-# Website to monitor
 URL = "https://sofed.tripura.gov.in/recruitment"
 
-# Telegram credentials
 BOT_TOKEN = "8666787831:AAGrZyh8QJu2vOmPY61XcyTNUffoLY_YWBo"
 CHAT_ID = "1199265793"
 
-
-def send_telegram_alert(alert_text):
-    telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-
-    payload = {
-        "chat_id": CHAT_ID,
-        "text": alert_text
-    }
-
-    requests.post(telegram_url, data=payload)
+FILE = "page.txt"
 
 
-def fetch_page_text():
-    response = requests.get(URL)
-    soup = BeautifulSoup(response.text, "html.parser")
+def send_alert(msg):
+    requests.post(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        data={"chat_id": CHAT_ID, "text": msg}
+    )
+
+
+def get_page():
+    r = requests.get(URL)
+    soup = BeautifulSoup(r.text, "html.parser")
     return soup.get_text()
 
 
-print("SOFED recruitment monitor started...")
+print("Checking website page please wait...")
 
-# Get initial page content
-previous_content = fetch_page_text()
+current = get_page()
 
-# Send startup message
-send_telegram_alert("SOFED Job Monitor Started")
+old = ""
+if os.path.exists(FILE):
+    old = open(FILE, "r", encoding="utf-8").read()
 
-while True:
-    time.sleep(3600) # 1 hour
+if current != old:
+    send_alert(" New update detected on website!\nBot developed by infoanupampal@gmail.com\nCheck here  :\nhttps://sofed.tripura.gov.in/recruitment")
+    print("Update detected")
 
-    print("Checking for updates...")
-
-    current_content = fetch_page_text()
-
-    if current_content != previous_content:
-        alert_message = "New update detected\nBot developed by infoanupampal@gmail.com\nCheck here:\nhttps://sofed.tripura.gov.in/recruitment"
-
-        send_telegram_alert(alert_message)
-
-        print("Update found. Telegram alert sent.")
-
-        previous_content = current_content
-
-    else:
-        print("No new updates.")
+open(FILE, "w", encoding="utf-8").write(current)
